3227a49d38b396e0c4f68ab1142a32f03b0c1e7b
projet3-pay-api <thomas.aaron.38off@gmail.com>
2025-12-11 15:14:32 +0100

    feat

--- public/main.js
+++ public/main.js
@@ -46,6 +46,27 @@ start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
 start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
 start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
 start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start    https://www.youtube.com/watch?v=dQw4w9WgXcQ
 echo **********************************************
 pause
 `;
